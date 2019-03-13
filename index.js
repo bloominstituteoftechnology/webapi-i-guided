93bc4e0d2f0929d0c54cbddab67af4b907520517
thomasutsey <tomutsey@yahoo.com>
2019-03-13 16:52:33 -0400

    Completed guided demo

--- index.js
+++ index.js
@@ -1,6 +1,74 @@
-const express = require ('express');
-const server = express();
-const port = 9000;
+const express = require ('express'); // this is similar to the import in react
 
-server.use('/api/hubs',hubs);
-server.get('/')
+const server = express();//this creates a new http server
+
+server.use(express.json());
+
+const db = require ('./data/db')
+
+
+// Handle retrieving the database
+server.get('/hubs',(req,res)=>{
+   db.hubs.find().
+   then((hubs)=>{
+    res.status(200).json({success:true,hubs})
+  })
+  .catch(({code, message}) =>{
+    res.status(code).json({success:false,message})
+  })
+})
+
+// Handle create
+
+server.post('/hubs',(req,res) =>{
+const hub = req.body;
+
+  db.hubs
+  .add(hub)
+  .then(hub =>{
+    res.status(201).json({success:true,hub})
+    console.log(posting)
+  })
+  .catch(({code, message}) =>{
+    res.status(code).json({success:false,message})
+  })
+})
+
+//Handle Delete 
+
+server.delete('/hubs/:id',(req,res) =>{
+  const hubId = req.params.id;
+
+  db.hubs
+
+  .remove(hubId)
+
+  .then(delID =>{res.status(200).json(delID);})
+
+  .catch(({code, message}) =>{
+    res.status(code).json({success:false,message})
+  })
+})
+// Handle Update
+server.put('/hubs/:id',(req,res) =>{
+  const { id } = req.params;
+  const updates = req.body;
+
+  db.hubs
+  
+  .update(id,updates)
+
+  .then (upd =>{
+    console.log (upd)
+    upd?res.status(200).json({success:true,upd})
+    : res.status(404).json({success:false, message:'invalid hub does not exist as requested'})
+  }) // checks to see if reffered data is an existing entree
+
+
+  .catch(({code, message}) =>{
+    res.status(code).json({success:false,message})
+  })
+})
+
+
+server.listen(5000,()=> console.log ('/Running on port 5000'));
